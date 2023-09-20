675ae66d309266105b585f81fbe0968206acb5b6
Fate <119829254+Master0fFate@users.noreply.github.com>
2023-09-20 18:18:33 +0200

    Main File Upload

---
+++ Fix_RadeonSoftware.ps1
@@ -0,0 +1,18 @@
+# Check if the script is running with admin privileges
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # Restart the script with admin permissions
+    Start-Process PowerShell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$($MyInvocation.MyCommand.Path)`"" -Verb RunAs
+    exit
+}
+
+$TaskName = "RadeonSoftwareAutostart"
+$TaskDescription = "Autostart Radeon Software as admin on PC startup"
+$TaskPath = "\"
+$TaskExecutable = "C:\Program Files\AMD\CNext\CNext\RadeonSoftware.exe" # Change this if you changed AMD Installation Path
+
+$Action = New-ScheduledTaskAction -Execute $TaskExecutable
+$Trigger = New-ScheduledTaskTrigger -AtLogon
+$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Hidden -Compatibility Win10
+$Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+Register-ScheduledTask -TaskName $TaskName -Description $TaskDescription -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal -TaskPath $TaskPath
